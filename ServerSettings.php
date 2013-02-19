681cb8d40cd238ca3df3e9b2730fd368b8ab9549
Stas Fomin <stas-fomin@yandex.ru>
2013-02-20 03:05:29 +0400

    add LDAP and other RS internl settings

--- ServerSettings.php
+++ ServerSettings.php
@@ -47,3 +47,56 @@ $wgEnableScaryTranscluding = true;
 
 // Bug 107222 - TikaMW. TODO: enable also on Windows along with Sphinx
 require_once($IP.'/extensions/TikaMW/TikaMW.php');
+
+///////////////////////////////////////////////
+# Allow to override per-page and per-category rights
+$haclgCombineMode = HACL_COMBINE_OVERRIDE;
+
+$wgGroupPermissions['user']['skipcaptcha'] = true; 
+$wgCaptchaTriggers['badlogin']      = true;
+
+$wgGroupPermissions['*']['interwiki'] = false;
+$wgGroupPermissions['sysop']['interwiki'] = true;
+
+# Pretect this wiki from viewing by unregs
+# Disable reading by anonymous users
+# (already disabled by IntraACL, but let's repeat just in case)
+$wgGroupPermissions['*']['read'] = false;
+$wgGroupPermissions['*']['edit'] = false;
+# But allow them to read e.g., these pages:
+$wgWhitelistRead = array("Main Page", "Special:UserLogin", "Special:Userlogin", "Help:Contents", "-");
+
+# Allow to upload not only the "wanted" documents
+$wgStrictFileExtensions = false;
+# Extend "wanted" document list
+$wgFileExtensions = array_merge($wgFileExtensions, array('odt','odp','ods','xlsx','docx','pptx','3ga'));
+
+require_once("$IP/extensions/Propiska/Propiska.php");
+
+# Open public logs
+$egFavRatePublicLogs = true;
+$egFavRateLogVisitors = true;
+
+#$wgMainCacheType=CACHE_NONE;
+#$wgMessageCacheType=CACHE_DB;
+
+$wgLocalisationCacheConf = array(
+  'class' => 'LocalisationCache',
+  'store' => 'db',
+); 
+
+$wgMimeTypeBlacklist = array(
+	# HTML may contain cookie-stealing JavaScript and web bugs
+	'text/javascript', 'text/x-javascript',  'application/x-shellscript',
+	# PHP scripts may execute arbitrary code on the server
+	'application/x-php', 'text/x-php',
+	# Other types that may be interpreted by some servers
+	'text/x-python', 'text/x-perl', 'text/x-bash', 'text/x-sh', 'text/x-csh',
+	# Client-side hazards on Internet Explorer
+	'text/scriptlet', 'application/x-msdownload',
+	# Windows metafile, client-side vulnerability on some systems
+	'application/x-msmetafile',
+);
+ 
+$wgDisableUploadScriptChecks = true; 
+
