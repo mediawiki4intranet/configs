02084db235161f591f83914f7edc693c57eca28e
Vitaliy Filippov <vitalif@yourcmc.ru>
2013-10-23 19:20:23 +0400

    Enable Variables by default

--- BaseSettings.php
+++ BaseSettings.php
@@ -205,6 +205,7 @@ require_once($IP.'/extensions/SlimboxThumbs/SlimboxThumbs.php');
 require_once($IP.'/extensions/Spoil/Spoil.php');
 require_once($IP.'/extensions/Duplicator/Duplicator.php');
 require_once($IP.'/extensions/PopupWhatlinkshere/PopupWhatlinkshere.php');
+require_once($IP.'/extensions/Variables/Variables.php');
 
 # Drafts
 require_once($IP.'/extensions/Drafts/Drafts.php');
